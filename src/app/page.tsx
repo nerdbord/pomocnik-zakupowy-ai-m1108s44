7078b3d4c39d91407a59568e66a0f5224a6ffa0f
Michal Lata <michallata.dev@gmail.com>
2024-09-14 03:52:01 +0200

    feat: Add FAQs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/Hero";
+import { FAQs } from "@/components/sections/FAQs";
 import { Features } from "@/components/sections/Features";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-24">
+    <div className="flex flex-col">
       <Hero />
       <Features />
+      <FAQs />
     </div>
   );
 }
