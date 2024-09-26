627f0675fb3fa883b73700c3be99f03e3f708875
Adrian <klimasa98@gmail.com>
2024-09-26 22:35:37 +0200

    fix types problem

--- src/lib/ebay/ebayService.ts
+++ src/lib/ebay/ebayService.ts
@@ -1,10 +1,14 @@
 // lib/ebay/ebayService.js
-const fetchEbayProducts = async (query, filter) => {
+const fetchEbayProducts = async (
+  query: string | number | boolean,
+  filter: string,
+) => {
   const endpoint = "https://api.ebay.com/buy/browse/v1/item_summary/search"; // Your eBay search endpoint
   const apiKey = process.env.EBAY_TOKEN; // Store your eBay API key in .env
   console.log(`Searching ${query} in Ebay API by filter ${filter}`);
   const url = `${endpoint}?q=${encodeURIComponent(query)}&limit=3&filter=${filter}`; // Modify parameters as needed
   console.log("URL FROM EBAYSERVICE: ", url);
+
   const headers = {
     Authorization: `Bearer ${apiKey}`,
     "Content-Type": "application/json",
@@ -14,7 +18,6 @@ const fetchEbayProducts = async (query, filter) => {
 
   try {
     const response = await fetch(url, { headers });
-    // console.log("RESPONSE FROM SERVICE: " + response.json());
 
     if (!response.ok) {
       throw new Error("Failed to fetch products from eBay");
@@ -23,8 +26,9 @@ const fetchEbayProducts = async (query, filter) => {
     const data = await response.json();
 
     console.log("Found items: ", data);
+
     // Example of formatting the response
-    const products = data.itemSummaries.map((item) => ({
+    const products = data.itemSummaries.map((item: { title: string; price: { value: number; currency: number; }; itemWebUrl: string; image: { imageUrl: string; }; }) => ({
       title: item.title,
       price: item.price.value,
       currency: item.price.currency,
@@ -34,8 +38,13 @@ const fetchEbayProducts = async (query, filter) => {
 
     return products;
   } catch (error) {
-    console.error("eBay API Error:", error.message);
-    throw error;
+    // Rzutuj 'error' na typ Error, aby uzyskać dostęp do właściwości 'message'
+    if (error instanceof Error) {
+      console.error("eBay API Error:", error.message);
+    } else {
+      console.error("Unexpected error occurred:", error);
+    }
+    throw error; // Rzucamy błąd dalej, aby mógł być obsłużony przez funkcje wywołujące
   }
 };
 
