9aad578e0db6f1c73abc19a0e1b969c5a00cf1eb
Michal Lata <michallata.dev@gmail.com>
2024-09-26 16:21:46 +0200

    fix: Add starting new chat fix

--- src/components/chat/ChatSection.tsx
+++ src/components/chat/ChatSection.tsx
@@ -49,7 +49,7 @@ export const ChatSection = () => {
         },
       ]);
       setNewChat(false);
-      setIsSearching(false);
+      setIsOfferShown(false);
     }
   }, [isNewChat, setMessages, setNewChat]);
 
